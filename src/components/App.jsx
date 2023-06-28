904572a0d9c938ac05e7de4d5e3c8421276fbe2c
Djoult <55453354+Djoult@users.noreply.github.com>
2023-06-28 18:37:15 +0300

    Adding routes logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,34 @@
+import { Route, Routes } from 'react-router-dom';
 import { ContactForm, Filter, Contacts } from 'components/index';
+
 import { H1, H2 } from './App/App.styled';
 
 export function App() {
-  return (
-    <>
-      <H1>PhoneBook</H1>
-      <ContactForm />
-      <H2>Contacts</H2>
-      <Filter />
-      <Contacts />
-    </>
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<Register />} />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<Login />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<Contacts />} />
+          }
+        />
+      </Route>
+    </Routes>
   );
 }
